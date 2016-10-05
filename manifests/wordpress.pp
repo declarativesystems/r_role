3393f0d46faec274d6c9fe7a2ae725b1c15700ed
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-05 17:17:45 +1100

    components for wordpress role

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,4 +1,6 @@
 class r_role::wordpress {
   include r_profile::base
+  include r_profile::database::mysql_server
+  include r_profile::web_service::apache
   include r_profile::webapp::wordpress
 }
