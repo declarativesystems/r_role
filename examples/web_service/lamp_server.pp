52d60d772691d8e96ab7f6af913f66532854c445
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-23 15:50:27 -0700

    lamp server role

---
+++ examples/web_service/lamp_server.pp
@@ -0,0 +1,20 @@
+class { 'r_profile::web_service::apache': }
+$db = { 
+  'quote' => {
+    user     => 'quote',
+    password => 'quote',
+    host     => 'localhost',
+    grant    => ['CREATE', 'SELECT', 'UPDATE', 'INSERT', 'DELETE'],
+  }
+}
+
+class { 'r_profile::database::mysql_server': 
+  db => $db,
+}
+class { 'r_profile::webapp::git_site':
+  sites  => {
+    '/var/www/html' => {
+      source => 'https://github.com/GeoffWilliams/phpquote',
+    }
+  }
+}
--- manifests/web_service/lamp_server.pp
+++ manifests/web_service/lamp_server.pp
@@ -1,5 +1,7 @@
 class r_role::web_service::lamp_server {
   include r_profile::base
+  include r_profile::database::mysql_server
   include r_profile::web_service::apache
   include r_profile::webapp::git_site
+  include r_profile::webapp::php_config
 }
