af0338c7a91c8b1606e8e73a856032b0f47441c9
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-09 11:09:04 +1100

    rewrite puppet master role for split functionality

--- manifests/puppet/master.pp
+++ manifests/puppet/master.pp
@@ -1,4 +1,9 @@
 class r_role::puppet::master {
   include r_profile::base
   include r_profile::puppet::master
+  include r_profile::puppet::agent_installers
+  include r_profile::puppet::hiera
+  include r_profile::puppet::git_server
+  include r_profile::puppet::auto_sign
+
 }
