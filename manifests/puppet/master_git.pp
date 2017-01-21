a351f14747b416bb4fb1c6ae1539f9bbf8579604
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-21 14:23:11 +1100

    choice of different preconfigured master roles

--- manifests/puppet/master_git.pp
+++ manifests/puppet/master_git.pp
@@ -1,8 +1,9 @@
-class r_role::puppet::master {
+class r_role::puppet::master_git {
   include r_profile::base
   include r_profile::puppet::master
   include r_profile::puppet::master::agent_installers
   include r_profile::puppet::master::hiera
+  include r_profile::puppet::master::git_server
   include r_profile::puppet::master::autosign
 
 }
---
+++ manifests/puppet/master_minimal.pp
@@ -0,0 +1,4 @@
+class r_role::puppet::master_minimal {
+  include r_profile::base
+  include r_profile::puppet::master
+}
