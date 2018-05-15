63bbdea597388bdde56f0dde1765f916bcddafa3
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-05-16 09:14:16 +1000

    removed nasty hack

--- manifests/puppet/master.pp
+++ manifests/puppet/master.pp
@@ -4,12 +4,7 @@
 # autosigning, heira/eyaml and the most popular agent installers
 class r_role::puppet::master {
 
-  # nasty hack..
-  class { "r_profile::puppet::master::forge_workaround":
-    forge_settings => {
-      'baseurl' => 'https://forgeapi.puppetlabs.com',
-    }
-  }
+
   include r_role::puppet::master_minimal
 
   include r_profile::puppet::master::agent_installers
