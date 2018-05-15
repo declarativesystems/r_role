3d22e2d38aafb726998ca6105d5f0f2a1326ef12
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-05-15 14:53:52 +1000

    try nasty hack

--- manifests/puppet/master.pp
+++ manifests/puppet/master.pp
@@ -3,6 +3,11 @@
 # An bigger and better puppet master with built-in support for a base profile,
 # autosigning, heira/eyaml and the most popular agent installers
 class r_role::puppet::master {
+
+  # nasty hack..
+  class { "r_profile::puppet::master::forge_workaround":
+    'baseurl' => 'https://forgeapi.puppetlabs.com'
+  }
   include r_role::puppet::master_minimal
 
   include r_profile::puppet::master::agent_installers
