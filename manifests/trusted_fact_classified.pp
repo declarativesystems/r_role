f6584750ba0749d88c8f08c3523d8a279e4e9ff6
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-11-22 17:03:36 +1100

    dont use notify

--- manifests/trusted_fact_classified.pp
+++ manifests/trusted_fact_classified.pp
@@ -1,8 +1,9 @@
 # R_role::trusted_fact_classified
 #
-# Classify a system based on the presence of the trusted fact `pp_role`. If the role
-# class specified does not exist, then include the base profile instead and print a
-# warning message. If no `pp_role` is set, then just include the base profile.
+# Classify a system based on the presence of the trusted fact `pp_role`. If the
+# role class specified does not exist, then include the base profile instead and
+# print a warning message to the client. If no `pp_role` is set, then just
+# include the base profile.
 #
 # @see https://forge.puppet.com/geoffwilliams/includable
 #
@@ -11,8 +12,8 @@
 #     include r_role::trusted_fact_classified
 #   }
 #
-#   # Alternatively, create a single group in the Puppet Enterprise Node Classifier
-#   # and assign this class to it.
+#   # Alternatively, create a single group in the Puppet Enterprise Node
+#   # Classifier and assign this class to it.
 #
 # @example Hiera data for selecting a different fallback (base) role
 #   r_role::trusted_fact_classified::base_role_class: 'role::disable_vm'
@@ -20,9 +21,10 @@
 # @example Hiera data to disable printing a warning message when pp_role class missing
 #   r_role::trusted_fact_classified::warning: false
 #
-# @param base_role_class The fallback role to use if no `pp_role` is set/avaiable
-# @param warning If requested class does not exist we will print a warning message
-#   informing the operator of this unless this parameter is set `false
+# @param base_role_class The fallback role to use if no `pp_role` is
+#   set/avaiable
+# @param warning If requested class does not exist we will print a warning
+#   message informing the operator of this unless this parameter is set `false`
 class r_role::trusted_fact_classified(
     String  $base_role_class  = "r_role::base",
     Boolean $warning          = true,
@@ -36,9 +38,7 @@ class r_role::trusted_fact_classified(
       if $warning {
         # display message on terminal when run interactively with `puppet agent -t`. This will also show up
         # in puppet console on the `log` tab of a report
-        notify { "Unable to assign role `${pp_role}`: class does not exist - falling back to `${base_role_class}`":
-          loglevel => warning,
-        }
+        warning("Unable to assign role `${pp_role}`: class does not exist - falling back to `${base_role_class}`")
       }
       $role_class = $base_role_class
     }
